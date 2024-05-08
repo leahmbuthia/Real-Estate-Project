ffc2117131b6219e09b507d759ca406e5f25a1fe
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-08 13:36:07 +0300

    Error Handler function file created

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,19 +1,19 @@
 import User from "../models/user.model.js";
-import bycrypt from 'bcrypt'
+import bycrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
 
-export const UserAuthentication = async(req,res) =>{
-const {username, email, password} =req.body;
-// ten is the salt value that makes the password encybted
-const hashedPassword = bycrypt.hashSync(password,10);
-// create it to the database and save to the databse
-const newUser =new User({username, email, password:hashedPassword });
-// it takes time we use await to not to get an error when waiting
+export const UserAuthentication = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  // ten is the salt value that makes the password encybted
+  const hashedPassword = bycrypt.hashSync(password, 10);
+  // create it to the database and save to the databse
+  const newUser = new User({ username, email, password: hashedPassword });
+  // it takes time we use await to not to get an error when waiting
 
-try {
-    await newUser.save()
-    res.status(201).json('user Created successfully') 
-} catch (error) {
-   res.status(500).json(error.message); 
-}
-
+  try {
+    await newUser.save();
+    res.status(201).json("user Created successfully");
+  } catch (error) {
+    next(errorHandler(550, "error from the function"));
+  }
 };
--- Api/index.js
+++ Api/index.js
@@ -22,6 +22,15 @@ app.use(express.json())
 
 app.use('/api', userRouter);
 app.use('/api', authRouter);
+app.use((err, req,res,next) =>{
+    const statusCode =err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 app.listen(3000, () => {
     console.log(`Server running on port http://localhost:3000`);
