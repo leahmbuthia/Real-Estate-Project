bec6ece2f8ca430deb291c7f2d2424abbd731169
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-13 23:09:26 +0300

    making the user profile protected route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,18 +5,29 @@ import Home from './component/pages/Home'
 import SignUp from './component/pages/SignUp'
 import About from './component/pages/About'
 import Profile from './component/pages/Profile'
+import Header from './component/Header'
+import PrivateRoute from './component/PrivateRoute'
 
 function App() {
  
 
   return (
     <BrowserRouter>
+     <Header/>
     <Routes>
+  
     <Route path='/' element={<Home/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
     <Route path='/signUp' element={<SignUp/>}/>
     <Route path='/about' element={<About/>}/>
-    <Route path='/profile' element={<Profile/>}/>
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          {/* <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          /> */}
+        </Route>
     </Routes>
     </BrowserRouter>
   )
