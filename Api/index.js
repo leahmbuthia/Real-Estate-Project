08c2d801f8d4c1edb2ba05b3202697a1f185520e
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-17 12:06:48 +0300

    create Listing api route

--- Api/index.js
+++ Api/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing',listingRouter);
 
 app.use((err, req,res,next) =>{
     const statusCode =err.statusCode || 500;
---
+++ Api/routes/listing.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createListing } from '../controllers/listing.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const listingRouter = express.Router();
+
+listingRouter.post('/create',verifyToken, createListing);
+
+export default listingRouter;
