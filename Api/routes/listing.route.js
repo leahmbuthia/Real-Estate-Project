0583eda22ae0e62b8cf2971bcc19cf2804c7e922
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-17 16:16:49 +0300

    Complete Delete User Listing Functionality

--- Api/routes/listing.route.js
+++ Api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const listingRouter = express.Router();
 
 listingRouter.post('/create',verifyToken, createListing);
-// listingRouter.post('/create',verifyToken, createListing);
+listingRouter.delete('/delete/:id',verifyToken, deleteListing);
 
 export default listingRouter;
--- Client/src/component/pages/Profile.jsx
+++ Client/src/component/pages/Profile.jsx
@@ -145,6 +145,24 @@ const Profile = () => {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl font-semibold text-center m-7">Profile</h1>
