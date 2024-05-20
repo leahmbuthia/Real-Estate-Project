7e687471245c4b3651959628cfeff3fea43c407a
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-20 14:39:23 +0300

    Complete Home page

--- Client/src/component/pages/Search.jsx
+++ Client/src/component/pages/Search.jsx
@@ -122,7 +122,7 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     const res = await fetch(`/api/listing/get?${searchQuery}`);
     const data = await res.json();
-    if (data.length < 9) {
+    if (data.length < 8) {
       setShowMore(false);
     }
     setListings([...listings, ...data]);
