841c9b68e8c2103b4dc8e3ee6e549f92171cf98d
Leah Nyambura <leahnyambura682@gmail.com>
2024-05-27 19:13:03 +0300

    depoly

--- Api/index.js
+++ Api/index.js
@@ -38,10 +38,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/Client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
